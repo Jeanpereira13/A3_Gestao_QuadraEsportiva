a7d033435a06877ab6ff94301be35719d37a2da2
Isabela Alves <isabelaiac.bela@gmail.com>
2026-05-26 13:23:19 -0300

    Adicionando meu código

---
+++ src/ControleAgendamento/ControleAgendamento.java
@@ -0,0 +1,72 @@
+package ControleAgendamento;
+
+import modeloEntidade.Agendamento;
+import java.util.ArrayList;
+
+public class ControleAgendamento {
+
+    public class ServicodeAgendamento {
+
+        //Arraylist como "Banco de dados" dinâmico
+        private ArrayList<Agendamento> listaAgendamento = new ArrayList<>();
+
+        //Cadastrar(Create)
+        public boolean cadastrar(Agendamento novo) {
+            //Verificação se existe um agendamento no mesmo dia e horário
+            for (Agendamento existente : listaAgendamento) {
+                if (existente.getDia().equalsIgnoreCase(novo.getDia()) &&
+                        existente.getHorario().equalsIgnoreCase(novo.getHorario())) {
+                    return false;//Se o dia e o horário escolhido já estiver reservado,o cadastro é barrado,retornando falso
+                }
+            }
+            //Se a quadra estiver livre,no dia e horário desejado,add na lista e retorna verdadeiro
+            listaAgendamento.add(novo);
+            return true;
+        }
+
+        //Listar(Read)
+
+        public ArrayList<Agendamento> listarTodos() {
+            //Apenas mostrar a lista cadastrada para a classe Main exibir
+            return listaAgendamento;
+        }
+
+        //Editar(Update)
+
+
+        public boolean editar(int id,String  novoDia, String novoHorario, String novoEsporte) {
+
+            //Verifica se o novo horário escolhido para a edição não vai colidir com outro agendamento existente no cadastro
+            for (Agendamento a : listaAgendamento) {
+                if (a.getId() != id) { //Ignora o próprio agendamento que será editado,o antigo
+                    if (a.getDia().equalsIgnoreCase(novoDia) && a.getHorario().equalsIgnoreCase(novoHorario)) {
+                        return false; // Se o dia e o horário escolhidos,tiverem ocupados por ua pessoa,o processo de edição é barrado
+                    }
+                }
+            }
+
+            //Se o horário estiver livre,localiza o agendamento pelo ID e altera os dados cadastrados(os atributos)
+            for (Agendamento a : listaAgendamento) {
+                if (a.getId() == id) {
+                    a.setDia(novoDia);
+                    a.setHorario(novoHorario);
+                    a.setTipoEsporte(novoEsporte);
+                    return true; // Alteração feita com sucesso
+                }
+            }
+            return false; // Retorna falso se o ID digitado não for encontrado,ou seja,a pessoa não fez um cadastro para poder editá-lo
+        }
+
+        //Excluir(Delete)
+        public boolean cancelar(int id) {
+            //Busca na lista quem tem o ID que solicitou a exclusão geral do cadastro,removendo da lista
+            for (Agendamento a : listaAgendamento) {
+                if (a.getId() == id) {
+                    listaAgendamento.remove(a);
+                    return true; // Remoção do cadastro concluída
+                }
+            }
+            return false; // Retorna falso se o ID não existir,ou seja, a pessoa não fez um cadastro para poder excluí-lo da lista
+        }
+    }
+}
---
+++ src/modeloEntidade/Agendamento.java
@@ -0,0 +1,70 @@
+package modeloEntidade;
+
+public class Agendamento {
+
+    private String nomeCliente;
+    private String horario;
+    private String dia;
+    private String TipoEsporte;
+    private int id;
+
+    //construtor, vai receber os dados
+    public Agendamento(String nomeCliente, String dia, String horario, String esporte, int id){
+     this.nomeCliente = nomeCliente;
+     this.dia = dia;
+     this.horario = horario;
+     this.TipoEsporte = esporte;
+     this.id = id;
+    }
+
+    //set altera o valor
+    public void setNomeCliente(String nomeCliente){
+        this.nomeCliente = nomeCliente;
+    }
+
+    //get pega o valor
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    public String getDia() {
+        return dia;
+    }
+
+    public void setTipoEsporte(String esporte) {
+        this.TipoEsporte = esporte;
+    }
+
+    public String getTipoEsporte() {
+        return TipoEsporte;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void mostrarDados(){
+        System.out.println("NOME: " + nomeCliente);
+        System.out.println("HORÁRIO: " + horario);
+        System.out.println("DIA: " + dia);
+        System.out.println("ESPORTE: " + TipoEsporte);
+        System.out.println("ID: " + id);
+    }
+
+}
